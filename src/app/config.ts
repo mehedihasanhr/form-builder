949a3a563795326a601d2a0334d9527200714cbd
MD Mehedi Hasan <mehedihasan.hr.324@gmail.com>
2025-04-20 11:34:11 +0600

    update

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,4 @@
 export const AppConfig = {
   API_URL:
-    "http://team.dev.helpabode.com:54292/api/wempro/react-dev/coding-test/devinfo.mehedi@gmail.com",
+    "https://team.dev.helpabode.com:54292/api/wempro/react-dev/coding-test/devinfo.mehedi@gmail.com",
 };
